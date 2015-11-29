be331c5a68b88f5a84dbd5ff86127374b16085d6
igorfama <igorfama10@live.com>
2015-11-28 22:10:01 -0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Constants;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Hello
+{
+    //Created by turkey for the EloBuddy Community! =)
+    internal class Program
+    {
+        
+            Chat.Print("<font color='#ffffff'>[00:00]</font> <font color='#4cc3ff'>turkey (IgorFama): <font color='#ffffff'>Hello World</font>");
+            Chat.Print("<font color='#ffffff'>[00:00]</font> <font color='#4cc3ff'>turkey (IgorFama): <font color='#ffffff'>My First Script</font>");
+            Chat.Print("<font color='#ffffff'>[00:01]</font> <font color='#4cc3ff'>turkey (IgorFama): <font color='#ffffff'>Igor here</font>");
+    }
+}
